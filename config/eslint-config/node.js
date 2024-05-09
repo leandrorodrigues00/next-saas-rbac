8a524afd78bd23120f2c039e522b5e71f6cca8cd
leandro <leandro.rodrigues00@hotmail.com>
2024-05-09 11:39:28 -0300

    :wrench: chore: Prettier & ESLint configuration in Monorepo

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,7 +1,7 @@
 /** @type { import('eslint').Linter.Config} */
 
 module.export = {
-  extends: ['@rocketseat/eslint-config/react'],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
