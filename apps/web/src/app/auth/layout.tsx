f23b79854e8d66da9527199aa252b7a8acd385a3
leandro <leandro.rodrigues00@hotmail.com>
2024-06-18 10:51:00 -0300

    ✨ feat: add authentication page

---
+++ apps/web/src/app/auth/layout.tsx
@@ -0,0 +1,11 @@
+type AuthLayoutProps = Readonly<{
+  children: React.ReactNode
+}>
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4">
+      <div className="w-full max-w-xs">{children}</div>
+    </div>
+  )
+}
