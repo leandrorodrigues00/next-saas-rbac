6db8b4feefa4ef13e742e8eecf85483866f0202a
leandro <leandro.rodrigues00@hotmail.com>
2024-06-28 10:27:17 -0300

    ✨ feat: add user login

--- apps/web/src/hooks/use-form-state.ts
+++ apps/web/src/hooks/use-form-state.ts
@@ -11,6 +11,7 @@ interface FormState {
 
 export function useFormState(
   action: (data: FormData) => Promise<FormState>,
+  onSuccess: () => Promise<void> | void,
   initialState?: FormState,
 ) {
   const [isPending, startTransition] = useTransition()
@@ -31,6 +32,10 @@ export function useFormState(
 
     startTransition(async () => {
       const state = await action(data)
+
+      if (state.success === true && onSuccess) {
+        await onSuccess()
+      }
 
       setFormState(state)
     })
